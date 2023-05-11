73d9404133821a7d07da446149fcea35f9133f93
Vishnu <vishnu@propelinc.com>
2023-05-11 19:00:15 +0530

    serialized json properties

--- Entity/Dtos/EventDto.cs
+++ Entity/Dtos/EventDto.cs
@@ -1,15 +1,19 @@
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 namespace Entity.Dtos
 {
   public class EventDto
   {
     [JsonPropertyName("event_name")]
+    [Required]
     public string EventName { get; set; } = string.Empty!;
 
     [JsonPropertyName("start_date_time")]
+    [Required]
     public DateTime StartDateTime { get; set; } = default;
 
     [JsonPropertyName("end_date_time")]
+    [Required]
     public DateTime EndDateTime { get; set; } = default;
   }
 }
